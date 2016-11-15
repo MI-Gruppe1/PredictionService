ffe0da802d229bf3f319f1e75f39d5185ab1f24d
Moritz Heindorf <moritz.heindorf@haw-hamburg.de>
2016-11-15 12:36:04 +0100

    First Commit

---
+++ PredictionService/src/service/Predictor.java
@@ -0,0 +1,19 @@
+package service;
+
+public class Predictor {
+
+	
+	/**
+	 * 
+	 * 
+	 * @return
+	 */
+	public int[] predict(){
+		int result[] = {0,0,0,0,0,0};
+		
+		
+		
+		return result;//KEK enough for the demo
+	}
+	
+}
